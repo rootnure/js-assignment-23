637b1d3f7b0ebf4a813bac6b0d183797a058aa97
rootnure <nur.diu.2791@gmail.com>
2023-08-09 04:43:47 +0600

    Code Indentation auto formated

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function cubeNumber(number) {
-    if(typeof number !== 'number'){
+    if (typeof number !== 'number') {
         return 'Please provide a number';
     }
     const cube = number * number * number;
@@ -7,32 +7,32 @@ function cubeNumber(number) {
 }
 
 function matchFinder(string1, string2) {
-    if(typeof string1 !== 'string' || typeof string2 !== 'string') {
+    if (typeof string1 !== 'string' || typeof string2 !== 'string') {
         return 'Both parameter must be in string format';
     }
-    if(string1.includes(string2)) {
+    if (string1.includes(string2)) {
         return true;
     }
     return false;
 }
 
 function sortMaker(arr) {
-    if(!Array.isArray(arr)){
+    if (!Array.isArray(arr)) {
         return 'Please provide an array';
     }
-    else if(arr.length !== 2) {
+    else if (arr.length !== 2) {
         return 'Please provide an array of two elements';
     }
-    else if(typeof arr[0] !== 'number' || typeof arr[1] !== 'number') {
+    else if (typeof arr[0] !== 'number' || typeof arr[1] !== 'number') {
         return 'Both element must be in number format';
     }
-    else if(arr[0] === arr[1]) {
+    else if (arr[0] === arr[1]) {
         return 'equal';
     }
-    else if(arr[0] <= 0 || arr[1] <= 0) {
+    else if (arr[0] <= 0 || arr[1] <= 0) {
         return 'Invalid Input';
     }
-    else if(arr[1] > arr[0]) {
+    else if (arr[1] > arr[0]) {
         const temp = arr[0];
         arr[0] = arr[1];
         arr[1] = temp;
@@ -42,14 +42,14 @@ function sortMaker(arr) {
 }
 
 function findAddress(obj) {
-    if(Array.isArray(obj) || typeof obj !== 'object') {
+    if (Array.isArray(obj) || typeof obj !== 'object') {
         return 'Please provide an object';
     }
     const keys = Object.keys(obj);
     const address = [];
     const addressParts = ['street', 'house', 'society'];
-    for(const part of addressParts) {
-        if(keys.includes(part)){
+    for (const part of addressParts) {
+        if (keys.includes(part)) {
             address.push(obj[part]);
             continue;
         }
@@ -57,27 +57,26 @@ function findAddress(obj) {
     }
     const fullAddress = address.join(',');
     return fullAddress;
-
 }
 
 function canPay(changeArray, totalDue) {
-    if(!Array.isArray(changeArray)) {
+    if (!Array.isArray(changeArray)) {
         return 'Please provide an array as first parameter';
     }
-    else if(changeArray.length === 0) {
+    else if (changeArray.length === 0) {
         return 'Array cannot be empty';
     }
     else if (typeof totalDue !== 'number' || totalDue <= 0) {
         return 'Please provide a positive number as second parameter';
     }
     let changeSum = 0;
-    for(const change of changeArray) {
-        if(typeof change !== 'number' || change < 0) {
+    for (const change of changeArray) {
+        if (typeof change !== 'number' || change < 0) {
             return 'All the values of array must be positive numbers';
         }
         changeSum += change;
     }
-    if(changeSum >= totalDue) {
+    if (changeSum >= totalDue) {
         return true;
     }
     return false;
